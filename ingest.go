d0fa3964a15b2b44309373134f253eb48ed2895f
Joe Egan <joeegan2202@gmail.com>
2020-07-01 13:12:19 -0400

    updated ingest to receive signatures

--- ingest.go
+++ ingest.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"crypto"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/hex"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -14,12 +22,22 @@ type IngestServer struct {}
 func (i *IngestServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Access-Control-Allow-Origin", "*")
 
+  query := r.URL.Query()
+
+  if query["signature"] == nil {
+    w.WriteHeader(http.StatusBadRequest)
+    w.Write([]byte("Incorrect parameters! Missing signature"))
+    return
+  }
+
+  signature := query["signature"][0]
+
   fmt.Println(r.URL.EscapedPath())
 
   cid := r.URL.EscapedPath()[:strings.LastIndex(r.URL.EscapedPath(), "/")]
   filename := r.URL.EscapedPath()[strings.LastIndex(r.URL.EscapedPath(), "/"):]
 
-  rows, err := db.Query("SELECT sid, room FROM cameras WHERE id=?;", cid)
+  rows, err := db.Query("SELECT cameras.sid, cameras.room, schools.publicKey FROM cameras INNER JOIN schools ON schools.id=cameras.sid WHERE cameras.id=?;", cid)
 
   if err != nil || !rows.Next() {
     w.WriteHeader(http.StatusBadRequest)
@@ -29,12 +47,48 @@ func (i *IngestServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
   defer rows.Close()
 
-  var room string
-  var sid string
+  var (
+    room string
+    sid string
+    publicKeyString string
+    publicKey *rsa.PublicKey
+  )
 
-  if err = rows.Scan(&sid, &room); err != nil {
+  if err = rows.Scan(&sid, &room, &publicKeyString); err != nil {
     w.WriteHeader(http.StatusInternalServerError)
     w.Write([]byte("Failed to get room from query!"))
+    return
+  }
+
+  publicKey, err = x509.ParsePKCS1PublicKey([]byte(publicKeyString))
+
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("Could not parse public key from database!"))
+    return
+  }
+
+  signData := make([]byte, 2048)
+
+  io.ReadFull(r.Body, signData)
+
+  hasher := sha256.New()
+
+  hasher.Write(signData)
+
+  signBytes, err := hex.DecodeString(signature)
+
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("Cannot decode hex data from signature parameter!"))
+    return
+  }
+
+  err = rsa.VerifyPKCS1v15(publicKey, crypto.SHA256, hasher.Sum(nil), signBytes)
+
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("Signature did not verify!"))
     return
   }
 
@@ -49,13 +103,95 @@ func (i *IngestServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     log.Fatal(err.Error())
   }
 
-  io.Copy(file, r.Body)
+  io.Copy(file, io.MultiReader(bytes.NewReader(signData), r.Body))
 }
 
 func receiveStatus(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Access-Control-Allow-Origin", "*")
-  
+
   query := r.URL.Query()
 
+  if query["signature"] == nil || query["cameraId"] == nil || query["status"] == nil {
+    w.WriteHeader(http.StatusBadRequest)
+    w.Write([]byte("Incorrect parameters! Missing parameters"))
+    return
+  }
 
+  signature := query["signature"][0]
+  cid := query["cameraId"][0]
+  status, err := strconv.Atoi(query["status"][0])
+
+  if err != nil {
+    w.WriteHeader(http.StatusBadRequest)
+    w.Write([]byte("Invalid value found for status!"))
+    return
+  }
+
+  rows, err := db.Query("SELECT schools.publicKey FROM cameras INNER JOIN schools ON schools.id=cameras.sid WHERE cameras.id=?;", cid)
+
+  if err != nil || !rows.Next() {
+    w.WriteHeader(http.StatusBadRequest)
+    w.Write([]byte("Could not get camera from database!"))
+    return
+  }
+
+  defer rows.Close()
+
+  var (
+    publicKeyString string
+    publicKey *rsa.PublicKey
+  )
+
+  if err = rows.Scan(&publicKeyString); err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("Failed to get room from query!"))
+    return
+  }
+
+  publicKey, err = x509.ParsePKCS1PublicKey([]byte(publicKeyString))
+
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("Could not parse public key from database!"))
+    return
+  }
+
+  hasher := sha256.New()
+
+  signData, err := ioutil.ReadAll(r.Body)
+
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("Could not read from body!"))
+    return
+  }
+
+  hasher.Write(signData)
+
+  signBytes, err := hex.DecodeString(signature)
+
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("Cannot decode hex data from signature parameter!"))
+    return
+  }
+
+  err = rsa.VerifyPKCS1v15(publicKey, crypto.SHA256, hasher.Sum(nil), signBytes)
+
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("Signature did not verify!"))
+    return
+  }
+
+  _, err = db.Exec("UPDATE cameras SET recording=? WHERE id=?;", status, cid)
+
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("Error updating camera record with status!"))
+    return
+  }
+
+  w.WriteHeader(http.StatusOK)
+  w.Write([]byte("Successfully updated camera record with status"))
 }
