002001cd4b9683f3d71ec6dc7ac77169aed5262b
Joe Egan <joeegan2202@gmail.com>
2020-07-02 21:50:33 -0400

    Added 'status' route

--- server.go
+++ server.go
@@ -65,6 +65,7 @@ func main() {
   r.HandleFunc("/admin/update/camera/", adminUpdateCamera)
   r.HandleFunc("/admin/delete/camera/", adminDeleteCamera)
   r.HandleFunc("/auth/", tempAuthorize)
+  r.HandleFunc("/status/", receiveStatus)
   r.HandleFunc("/request/", requestStream) // For admins/teachers/students who are requesting a video stream
   r.PathPrefix("/stream/").Handler(http.StripPrefix("/stream/", new(StreamServer))) // The actual file server for streams
   r.PathPrefix("/ingest/").Handler(http.StripPrefix("/ingest/", new(IngestServer))) // The actual file server for streams
