a139ae98fee5db4fe0f6b722f53862af18aa73d7
Joe Egan <joeegan2202@gmail.com>
2020-07-07 11:27:10 -0400

    Added more functionality to csv reader

--- schoolImport.go
+++ schoolImport.go
@@ -39,7 +39,30 @@ func importPeople(w http.ResponseWriter, r *http.Request) {
 
   dataSheet := csv.NewReader(r.Body)
 
+  // Get headers from csv
+  indices := make([]int, 4)
+  values, err := dataSheet.Read()
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte(`{"status": false, "err": "Could not read header line from csv"}`))
+    return
+  }
+
+  for i, value := range values {
+    switch value {
+    case "uname":
+      indices[0] = i
+    case "fname":
+      indices[1] = i
+    case "lname":
+      indices[2] = i
+    case "role":
+      indices[3] = i
+    }
+  }
+
+  // Write rest of data to db
   for record, err := dataSheet.Read(); err == nil; {
-    fmt.Println(record[0])
+    fmt.Println(record[indices[0]], record[indices[1]], record[indices[2]], record[indices[3]])
   }
 }
