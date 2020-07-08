c3e66aafa9ca1b5fc4a8b8ddfef3711ef5d86c48
Joe Egan <joeegan2202@gmail.com>
2020-07-08 10:31:17 -0400

    Added content type header

--- schoolImport.go
+++ schoolImport.go
@@ -10,6 +10,7 @@ func importPeople(w http.ResponseWriter, r *http.Request) {
   if r.Method == "OPTIONS" {
     w.Header().Set("Access-Control-Allow-Origin", "*")
     w.Header().Set("Access-Control-Allow-Methods", "PUT")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
     w.WriteHeader(http.StatusOK)
     return
   }
