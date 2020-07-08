980756df253ce3c1886dd2db9ca511b391cf10d2
Joe Egan <joeegan2202@gmail.com>
2020-07-08 10:28:14 -0400

    Added put to access control allow methods

--- schoolImport.go
+++ schoolImport.go
@@ -9,6 +9,7 @@ import (
 func importPeople(w http.ResponseWriter, r *http.Request) {
   if r.Method == "OPTIONS" {
     w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "PUT")
     w.WriteHeader(http.StatusOK)
     return
   }
