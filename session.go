00da0e3ec0a6cbb695c786eaf702a40b8103c735
Joe Egan <joeegan2202@gmail.com>
2020-07-13 13:12:32 -0400

    Added POST handling to passAuth

--- session.go
+++ session.go
@@ -21,6 +21,13 @@ func addSession(uname string, sid string) string {
 }
 
 func passAuth(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "OPTIONS" {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 
