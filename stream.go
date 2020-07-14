e450151e0ae7915cf7d3d5d62002682ec1b687cf
Joe Egan <joeegan2202@gmail.com>
2020-07-14 14:59:53 -0400

    Added initial streamInfo

--- stream.go
+++ stream.go
@@ -66,3 +66,55 @@ func (s *StreamServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.StripPrefix(fmt.Sprintf("%s/%s", sid, session), http.FileServer(http.Dir(fmt.Sprintf("%s/%s", os.Getenv("FS_PATH"), sid)))).ServeHTTP(w, r)
 	}
 }
+
+func streamInfo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+
+	query := r.URL.Query()
+
+	var session string
+	var sid string
+
+	if query["session"] == nil || query["sid"] == nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"status": false, "err": "Missing parameters"}`))
+		return
+	}
+
+	session = query["session"][0]
+	sid = query["sid"][0]
+
+	role, err := checkSession(sid, session)
+
+	if err != nil {
+		logger.Printf("Error in streamInfo trying to check session! Error: %s\n", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf(`{"status": false, "err": "Incorrect role for session"}`)))
+		return
+	}
+
+	switch role {
+	case "A", "S", "T":
+		row := db.QueryRow("SELECT people.fname, people.lname, classes.name, periods.code FROM sessions INNER JOIN people ON sessions.uname=people.uname INNER JOIN roster ON people.id=roster.pid INNER JOIN classes ON roster.cid=classes.id INNER JOIN periods ON classes.period=periods.code WHERE periods.stime<unix_timestamp() AND periods.etime>unix_timestamp() AND sessions.id=? AND sessions.sid=?;", session, sid)
+
+		var (
+			fname  string
+			lname  string
+			cname  string
+			period string
+		)
+
+		err := row.Scan(&fname, &lname, &cname, &period)
+
+		if err != nil {
+			logger.Printf("Error in streamInfo trying to scan for information! Error: %s\n", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf(`{"status": false, "err": "Error in streamInfo trying to scan for information!"}`)))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf(`{"status": false, "err": "", "info": {"fname": "%s", "lname": "%s", "cname": "%s", "period": "%s"}}`, fname, lname, cname, period)))
+	}
+}
