41e587220f2e1c4131a54e7fe249dfd811b4a297
Joe Egan <joeegan2202@gmail.com>
2020-07-27 12:01:34 -0400

    Removed http support

--- server.go
+++ server.go
@@ -74,7 +74,6 @@ func main() {
 	r.HandleFunc("/check/", handleCheck)
 	r.PathPrefix("/stream/").Handler(http.StripPrefix("/stream/", new(StreamServer))) // The actual file server for streams
 	r.PathPrefix("/ingest/").Handler(http.StripPrefix("/ingest/", new(IngestServer))) // The actual file server for streams
-	go logger.Fatal(http.ListenAndServe(":80", r))
 	certpath, err := os.Getwd()
 	logger.Fatal(http.ListenAndServeTLS(":443", fmt.Sprintf("%s/public.crt", certpath), fmt.Sprintf("%s/private.key", certpath), r))
 }
