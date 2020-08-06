97a3d441e8089a09c9b03df3171652373b15d75f
Joe Egan <joeegan2202@gmail.com>
2020-08-06 11:25:42 -0400

    Added noac to mount for better synchronization

--- docker-run.sh
+++ docker-run.sh
@@ -1,7 +1,7 @@
 apt update
 apt install nfs-common
 mkdir -p /mnt/nfs
-mount 10.116.0.8:/mnt/volume_nyc1_01 /mnt/nfs
+mount -o noac 10.116.0.8:/mnt/volume_nyc1_01 /mnt/nfs
 docker build -t edustream-server .
 docker run --publish 80:80 --detach -it \
     -v /mnt/nfs:/nfs \
